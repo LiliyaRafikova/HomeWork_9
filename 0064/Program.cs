8d29cf4f481b2d45d6dbf5805678cd454132ebc8
Лиля <@mvnet.local>
2022-09-02 16:09:59 +0500

    HomeWork_9

---
+++ 0064/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumber (int M,int N)
+{
+    if (N<M ) return ;
+    Console.Write(M +"  ");
+   PrintNumber(M+1,N);
+}
+
+PrintNumber(M,N);
---
+++ 0066/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void PrintSumm(int M, int N, int summa)
+{
+  summa = summa + N;
+  if (N <= M)
+  {
+    Console.Write($"Сумма элементов= {summa} ");
+    return;
+  }
+  PrintSumm(M, N - 1, summa);
+
+}
+
+
+if (M > N) 
+{
+ int temp = M;
+  M= N; 
+  N = temp;
+}
+
+PrintSumm(M, N, 0);
+
